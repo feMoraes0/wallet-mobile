dfce17d1f05d4b34a85959ff66934c1ea5dcb9bb
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-10-11 23:19:27 -0300

    feat(home): adds main card skeleton

--- src/screens/Home/style.tsx
+++ src/screens/Home/style.tsx
@@ -2,16 +2,20 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
     flex: 1,
-    flexDirection: 'column',
   },
   divider: {
+    alignSelf: 'center',
     backgroundColor: '#e0e4ff',
     borderRadius: 50.0,
     height: 6.0,
     marginVertical: 12.0,
     width: 45.0,
+  },
+  mainCard: {
+    height: 480.0,
+    marginHorizontal: 30.0,
+    borderRadius: 20.0,
   }
 });
 
--- src/screens/Home/template.tsx
+++ src/screens/Home/template.tsx
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import SettingsCard from './components/SettingsCard';
 import styles from './style';
 
@@ -9,6 +9,14 @@ const template = () => {
     <View style={styles.container}>
       <SettingsCard />
       <View style={styles.divider} />
+      <LinearGradient
+        start={{x: 0, y: 0}}
+        end={{x: 1, y: 1}}
+        colors={['#4c669f', '#3b5998', '#192f6a']}
+        style={styles.mainCard}
+      >
+        <View />
+      </LinearGradient>
     </View>
   );
 }
